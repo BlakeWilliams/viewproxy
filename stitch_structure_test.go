02d92e7b501120a5b790ad4df145da0ca51fb8b5
Blake Williams <blake@blakewilliams.me>
2021-10-03 21:48:51 -0400

    Add test for stitch structure

---
+++ stitch_structure_test.go
@@ -0,0 +1,43 @@
+package viewproxy
+
+import (
+	"testing"
+
+	"github.com/blakewilliams/viewproxy/pkg/fragment"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStitchStructure(t *testing.T) {
+	rootFragment := fragment.Define("layout", fragment.WithChildren(fragment.Children{
+		"header": fragment.Define("header"),
+		"body": fragment.Define("body", fragment.WithChildren(fragment.Children{
+			"main":    fragment.Define("main"),
+			"sidebar": fragment.Define("sidebar"),
+		})),
+	}))
+
+	structure := stitchStructureFor(rootFragment)
+	headerStructure := structure.DependentStructures()[0]
+	bodyStructure := structure.DependentStructures()[1]
+
+	require.Equal(t, "root", structure.Key())
+
+	require.Equal(t, "root.header", headerStructure.Key())
+	require.Equal(t, "header", headerStructure.ReplacementID())
+
+	require.Equal(t, "root.body", bodyStructure.Key())
+	require.Equal(t, "body", bodyStructure.ReplacementID())
+
+	var bodyKeys []string
+	var bodyReplacementIDs []string
+	for _, structure := range bodyStructure.DependentStructures() {
+		bodyKeys = append(bodyKeys, structure.Key())
+		bodyReplacementIDs = append(bodyReplacementIDs, structure.ReplacementID())
+	}
+
+	require.Contains(t, bodyKeys, "root.body.main")
+	require.Contains(t, bodyKeys, "root.body.sidebar")
+
+	require.Contains(t, bodyReplacementIDs, "main")
+	require.Contains(t, bodyReplacementIDs, "sidebar")
+}
