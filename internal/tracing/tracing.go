3848b71a0ab8fc7f20cd9a10c05f0f4bb81964a7
Kevin Sawicki <kevinsawicki@gmail.com>
2022-05-31 15:19:14 -0700

    Only require used parts of interface

--- internal/tracing/tracing.go
+++ internal/tracing/tracing.go
@@ -24,14 +24,6 @@ type TracingConfig struct {
 }
 
 type logger interface {
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
-	Fatalln(v ...interface{})
-	Panic(v ...interface{})
-	Panicf(format string, v ...interface{})
-	Panicln(v ...interface{})
-	Print(v ...interface{})
-	Printf(format string, v ...interface{})
 	Println(v ...interface{})
 }
 
